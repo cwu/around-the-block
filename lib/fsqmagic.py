e6e0de4e33d2bd1de1c004ca446e896a4ef82b19
Wien Leung <->
2013-02-02 08:17:23 -0500

    foursquare magic. probably works. ?

---
+++ lib/fsqmagic.py
@@ -0,0 +1,53 @@
+import string
+import urlparse
+import pprint
+from foursquare import Foursquare
+
+pp = pprint.PrettyPrinter(indent=2)
+
+class FsqMagic:
+  def __init__(self, oauth, explore={}, photos=[]):
+    self.f = Foursquare(access_token = oauth)
+    self.explore = explore
+    self.photos = photos
+
+  def magic(self, latlong, dist=1000, limit=50, verbose=False):
+    print "Explore\n"
+    # explore venues
+    explore = self.f.venues.explore(params={"ll":latlong,"friendVisits":"visited","radius":dist, "limit":limit, "section":"food"})["groups"][0]["items"]
+
+    if verbose == True:
+      print "Explore results:\n"
+      pp.pprint(explore)
+
+    # filter shitty results out
+    print "Filter\n"
+    filtered_explore = {}
+    for i in explore:
+      if i["reasons"]["count"] > 0:
+        # sort by places
+        filtered_explore[i["venue"]["name"]] = i
+
+    if verbose == True:
+      print "\nFiltered explore results:\n"
+      pp.pprint(filtered_explore)
+
+    self.explore = filtered_explore
+
+    # make photo list
+    print "Making photo list\n"
+    for key in self.explore.keys():
+      vid = self.explore[key]["venue"]["id"]
+      venue_photos = self.f.venues.photos(vid, params={"group":"venue"})["photos"]["items"]
+      for photo in venue_photos:
+        photo["venue_id"] = vid;
+        self.photos.append(photo)
+
+    if verbose == True:
+      print "\nPhoto list:\n"
+      pp.pprint(filtered_explore)
+
+    return self.explore
+
+  def get_photos(self, offset=0, limit=10):
+    return self.photos[offset:offset+limit]
