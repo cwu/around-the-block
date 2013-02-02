878ba6e72faf1442e7367f53847743dc8af89096
Chris Wu <me@chriswu.me>
2013-02-02 05:37:41 -0500

    add redis port ability

--- photos/photos.py
+++ photos/photos.py
@@ -3,10 +3,11 @@ from flask import request, Blueprint, g, make_response
 import json
 from lib.fbmagic import magic
 import redis
+import settings
 
 photos_blueprint = Blueprint('photos', __name__)
 
-r = redis.StrictRedis()
+r = redis.StrictRedis(port=settings.REDIS_PORT)
 
 @photos_blueprint.route('/photos')
 def photos():
@@ -16,7 +17,7 @@ def photos():
 
   oauth_token = g.user.fb_access_token
 
-  cache_key = "%s-%s-%s" % (g.user.id, latitude, longitude)
+  cache_key = "fb-results-%s-%s-%s" % (g.user.id, latitude, longitude)
   item = r.get(cache_key)
   if not item:
     item = json.dumps(magic(oauth_token, '%s,%s' % (latitude, longitude), dist=distance))
--- settings.py
+++ settings.py
@@ -5,3 +5,5 @@ STATIC_PATH = os.path.join(PROJECT_DIR, 'assets')
 
 FB_APP_ID = os.environ['AROUND_THE_BLOCK_FB_APP_ID']
 FB_SECRET = os.environ['AROUND_THE_BLOCK_FB_SECRET']
+
+REDIS_PORT = os.environ.get('AROUND_THE_BLOCK_REDIS_PORT', 6379)
