b7c9cbd2287a7aca1ff880f757a7508fb32ca169
Chris Wu <me@chriswu.me>
2013-02-02 01:12:33 -0500

    simplify db

--- db.py
+++ db.py
@@ -11,5 +11,5 @@ def init():
   # import all modules here that might define models so that
   # they will be registered properly on the metadata. Otherwise
   # you will have to import them first before calling init_db()
-  from models import user
+  import models
   Base.metadata.create_all(bind=engine)
