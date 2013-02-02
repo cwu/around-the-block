c0e02f8d65988bdbf71750bfd600eaee3dac7a98
Wien Leung <->
2013-02-02 02:11:55 -0500

    more robust, added page images

--- lib/fbmagic.py
+++ lib/fbmagic.py
@@ -5,19 +5,20 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=2)
 
-def magic(oauth, latlong, dist=1000, verbose=False):
+def magic(oauth, latlong, dist=1000, limit=5, verbose=False):
   g = GraphAPI(oauth)
 
   # search a bunch of stuff, until no more results
   # https://graph.facebook.com/search?type=location&center=[lat],[long]&distance=[dist]&access_token=[oauth]
   search = []
   params = {"type":"location", "center":latlong, "distance":dist, "access_token":oauth}
-  while True:
+  while limit > 0:
     res = g.request("search", params)
     if len(res.keys()) == 0 or not res.get("paging"):
       break
     search += res["data"]
     params = dict(urlparse.parse_qsl(res["paging"]["next"].split("?")[1]))
+    limit -= 1
 
   if verbose == True:
     pp.pprint(search)
@@ -25,11 +26,16 @@ def magic(oauth, latlong, dist=1000, verbose=False):
   # filter
   places = g.fql("SELECT name, location, type, page_id FROM page where page_id in (%s)" % string.join([i["place"]["id"] for i in search], ", "))
   desired_place_types = ["RESTAURANT/CAFE", "BAR", "CLUB", "FOOD/BEVERAGES", "LOCAL BUSINESS"]
+  limit = 1;
   filtered_places = {}
   for i in places:
     if i["type"].upper() in desired_place_types:
-      filtered_places[i["name"]] = i
-      filtered_places[i["name"]]["data"] = []
+      name = i["name"]
+      filtered_places[name] = i
+      filtered_places[name]["data"] = []
+      page = g.get_object(str(i["page_id"]) + "/picture", width="600")
+      if page.get("url"):
+        filtered_places[name]["photo_url"] = page["url"]
       del i["name"]
 
   if verbose == True:
@@ -41,11 +47,11 @@ def magic(oauth, latlong, dist=1000, verbose=False):
       filtered_places[i["place"]["name"]]["data"].append(i)
       del i["place"]
 
+      # get fb picture urls if can
       if i["type"] == "photo":
-        # get fb picture urls if can
-        i["photo_url"] = g.get_object(i["id"])["images"]
-      else:
-        # find pictures for items without pictures
+        photo = g.get_object(i["id"])
+        if photo.get("images"):
+          i["photo_url"] = photo["images"]
 
   if verbose == True:
     pp.pprint(filtered_places)
